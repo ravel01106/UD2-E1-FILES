8153e170be18f910dced9ba17bb11e21975d4f79
ravel01106 <amandaravelocabrera@gmail.com>
2023-11-06 18:17:43 +0000

    doc: commenting on the functions used

--- src/aync-await/objectToCSVAsync.ts
+++ src/aync-await/objectToCSVAsync.ts
@@ -1,7 +1,19 @@
-//import { promises as fsPromises } from 'fs'
 import { ObjectToCSV } from '../interfaces/objectToCSV'
 import { obtainContent } from '../helpers/obtainContent'
 import { writeToCSVAsync } from '../helpers/writeToCSV'
+
+/**
+ *
+ * @param params
+ * is a object with the necesary parameters:
+ * -> **jsonData**: is the array of the data.
+ * -> **fields**: the fields we want to write.
+ * -> **filePath**: is the path to the file to be created or modified.
+ * -> **filter**: is an optional parameter which performs a filtering
+ *    of the data.
+ * @returns
+ * a promise that return true if the file has been written properly or an error
+ */
 
 async function objectToCSVAsync<T>(params: ObjectToCSV<T>): Promise<boolean> {
   let { jsonData, fields, filePath, filter } = params
